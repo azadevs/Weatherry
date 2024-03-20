57c3007d88d14352a0f54c60384d374ed67fd579
Azamat <azamatdeveloper3@gmail.com>
2024-03-20 16:57:39 +0500

    Changed weather icons codes in Constants.kt fromWMO function.

--- app/src/main/java/android/azadev/weatherry/utils/Constants.kt
+++ app/src/main/java/android/azadev/weatherry/utils/Constants.kt
@@ -11,20 +11,20 @@ object Constants {
 
     const val BASE_URL = "http://api.weatherapi.com/v1/"
 
-     fun fromWMO(code: Int): Int {
+    fun fromWMO(code: Int): Int {
         return when (code) {
-            113 -> R.drawable.ic_sunny
-            116, 119, 143 -> R.drawable.ic_cloudy
-            122 -> R.drawable.ic_overcast
-            176 -> R.drawable.ic_sunnyrainy
-            179, 230, 323, 326, 368, 392, 395 -> R.drawable.ic_snowy
-            182, 185, 281, 284, 311, 317, 320, 350, 362, 365, 377 -> R.drawable.ic_snowyrainy
-            200 -> R.drawable.ic_thunder
-            227, 329, 332, 335, 338, 371, 374 -> R.drawable.ic_heavysnow
-            248, 260 -> R.drawable.ic_very_cloudy
-            263, 266, 299, 302, 353, 356, 359 -> R.drawable.ic_rainshower
-            293, 296, 305, 308, 314 -> R.drawable.ic_rainy
-            386, 389 -> R.drawable.ic_rainythunder
+            1000 -> R.drawable.ic_sunny
+            1003, 1006, 1030 -> R.drawable.ic_cloudy
+            1009 -> R.drawable.ic_overcast
+            1063 -> R.drawable.ic_sunnyrainy
+            1066, 1117, 1210, 1213, 1255, 1279, 1282 -> R.drawable.ic_snowy
+            1069, 1072, 1168, 1171, 1198, 1204, 1207, 1237, 1249, 1252, 1264 -> R.drawable.ic_snowyrainy
+            1087 -> R.drawable.ic_thunder
+            1114, 1216, 1219, 1222, 1225, 1258, 1261 -> R.drawable.ic_heavysnow
+            1135, 1147 -> R.drawable.ic_very_cloudy
+            1150, 1153, 1186, 1189, 1240, 1243, 1246 -> R.drawable.ic_rainshower
+            1180, 1183, 1192, 1195, 1201 -> R.drawable.ic_rainy
+            1273, 1276 -> R.drawable.ic_rainythunder
             else -> R.drawable.ic_sunny
         }
     }
