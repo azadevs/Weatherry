577738ec8c0e8141928c993f8724fc101394849a
Azamat <azamatdeveloper3@gmail.com>
2024-03-20 16:55:20 +0500

    Updated UI with weather details in DetailsDialogFragment.kt.

--- app/src/main/java/android/azadev/weatherry/ui/details/DetailsDialogFragment.kt
+++ app/src/main/java/android/azadev/weatherry/ui/details/DetailsDialogFragment.kt
@@ -2,6 +2,9 @@ package android.azadev.weatherry.ui.details
 
 import android.azadev.weatherry.R
 import android.azadev.weatherry.databinding.FragmentDialogDetailsBinding
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.navArgs
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 /**
@@ -10,9 +13,31 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
  */
 
 class DetailsDialogFragment : BottomSheetDialogFragment(R.layout.fragment_dialog_details) {
+
     private var _binding: FragmentDialogDetailsBinding? = null
     private val binding get() = _binding!!
 
+    private val args: DetailsDialogFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentDialogDetailsBinding.bind(view)
+
+        configureUI()
+
+    }
+
+    private fun configureUI() {
+        val details = args.details
+        binding.apply {
+            tvHumidity.text = details.humidity.toString()
+            tvPrecipitation.text = details.precipitation.toString()
+            tvPressure.text = details.pressure.toString()
+            tvUv.text = details.uv.toString()
+            tvVisibility.text = details.vis.toString()
+            tvWind.text = details.wind.toString()
+        }
+    }
 
 
     override fun onDestroyView() {
