8bfc3338253920b23c696eaba46b4cac5b8e9390
Azamat <azamatdeveloper3@gmail.com>
2024-03-22 12:33:56 +0500

    Removed toolbar in activity_weather.xml. Added some strings to strings.xml.

--- app/src/main/java/android/azadev/weatherry/ui/WeatherActivity.kt
+++ app/src/main/java/android/azadev/weatherry/ui/WeatherActivity.kt
@@ -1,22 +1,15 @@
 package android.azadev.weatherry.ui
 
-import android.azadev.weatherry.R
+import android.azadev.weatherry.databinding.ActivityWeatherBinding
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
 
 class WeatherActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWeatherBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_weather)
+        binding = ActivityWeatherBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setSupportActionBar(findViewById(R.id.toolbar))
-        setupActionBarWithNavController(findNavController(R.id.nav_host))
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host)
-        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
