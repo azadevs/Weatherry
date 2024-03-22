cd208112d86e2907f62c997ac37e2210171ea6a7
Azamat <azamatdeveloper3@gmail.com>
2024-03-22 14:09:29 +0500

    Added toolbar to activity_weather.xml.

--- app/src/main/java/android/azadev/weatherry/ui/WeatherActivity.kt
+++ app/src/main/java/android/azadev/weatherry/ui/WeatherActivity.kt
@@ -11,5 +11,7 @@ class WeatherActivity : AppCompatActivity() {
         binding = ActivityWeatherBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+
     }
 }
